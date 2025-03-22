5667b825b12a6bd85a57b06ace31f60db4b81f17
nvikramraj <nvikramraj1305@gmail.com>
2025-03-21 20:49:07 -0500

    header fix

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,13 +5,19 @@ import { motion } from "motion/react"
 
 const Header = ({isDarkMode}) => {
   return (
-    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
-      <motion.div
+
+    <div className='w-11/12 max-w-3xl text-center mx-auto min-h-screen flex flex-col items-center justify-start gap-4 pt-24 pb-12'>
+      <motion.div 
         initial={{scale: 0}}
         whileInView={{scale: 1}}
         transition={{duration:0.8, type:'spring', stiffness: 100}}
+        className="mt-8"
       >
-        <Image src={assets.vikram_user_img} alt='' className='rounded-full w-32' />
+        <Image 
+          src={assets.vikram_user_img} 
+          alt='Profile' 
+          className='rounded-full w-32 h-32 object-cover'
+        />
       </motion.div>
       <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>Vikram Raj Nagoor Kani </h3>
       <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>M.Eng Autonomy and Robotics, University of Illinois Urbana-Champaign </h1>
