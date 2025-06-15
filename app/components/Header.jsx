72f67e77cc0d1bfd0d2feeda375b5bc182b9f252
nvikramraj <nvikramraj1305@gmail.com>
2025-06-14 21:53:35 -0500

    Updated Resume

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = ({isDarkMode}) => {
       <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
         <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>
           Contact Me <Image src={assets.right_arrow_white} alt='' className='w-4' /></a>
-        <a href="/Vikram_Resume_perception.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black' >
+        <a href="/Vikram_Resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black' >
         My Resume <Image src={assets.download_icon} alt='' className='w-4' /></a>
       </div>
       
