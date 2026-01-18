5176461368f1c233c69aeacd124902c5d2e76dbf
Vikram Raj <54244791+nvikramraj@users.noreply.github.com>
2026-01-18 16:20:11 -0600

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -129,7 +129,7 @@ export const workData = [
 
 export const timeData = [
     {
-        title: 'UIUC College of Veterinary Medicine',
+        title: 'Premier Cooperative Inc',
         time:'Aug 2025 - Present',
         role:'Robotics Systems Integration Engineer Intern',
         description: ['Designing a ROS 2 based automation pipeline hosted in Docker Container for grain quality inspection, integrating multi-sensor systems with existing grain probe infrastructure improving speed, cost, accuracy, hours of operation and safety compared to manual operation of the probe.'
